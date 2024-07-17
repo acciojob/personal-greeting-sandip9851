9410a0f51e1ed6b808fcfd0116b7b769a4abcd75
sandip <mr.sandippradhan@gmail.com>
2024-07-17 08:14:41 +0530

    finshed

--- src/components/input.jsx
+++ src/components/input.jsx
@@ -1,22 +1,22 @@
 import React from 'react'
 import { useState } from 'react'
 
-function input() {
-  let [name , setName ] = useState("")
-  let checkInput = (e)=>{
-    if(e.target.value == ""){
-      setName("")
-    }else{
-      setName(`Hello ${e.target.value}`)
-    }
-  }
+function input() { const [name, setName] = useState("");
+
+  const onChangeEvent = (event) => {
+    setName(`Hello ${event.target.value}!`);
+  };
+
   return (
-    <>
-    <p>Enter your name:</p>
-    <input type="text" id='name' onChange={(e)=>checkInput(e)}/>
-    <p>{name}</p>
-    </>
-  )
-}
+    <div>
+      {
+        <>
+          <input type="{text}" onChange={onChangeEvent} />
+          <p className="display">{name}</p>
+        </>
+      }
+    </div>
+  );
+};
 
 export default input
